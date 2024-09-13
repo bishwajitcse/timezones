30457f21c0df2f86aea8b466620a52e7e6f9ebd7
Bishwajit Barua <bjaddt@gmail.com>
2024-09-13 18:10:16 +0000

    Initial commit

---
+++ src/test/java/com/bjb/timezones/TimezonesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjb.timezones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimezonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
